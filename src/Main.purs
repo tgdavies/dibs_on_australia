5044088cdd88381885578bb058ce6542260ae0d7
Tom Davies <tdavies@atlassian.com>
2015-08-23 19:18:29 +1000

    use functions for messages

--- src/Main.purs
+++ src/Main.purs
@@ -21,58 +21,89 @@ import Control.Bind
 
 
 
-type ChanceDeck = { title :: String, cards :: Array String }
+type ChanceDeck = { title :: String, cards :: Array (forall eff. Eff (random :: RANDOM | eff) String) }
+
+s :: String -> forall eff. Eff (random :: RANDOM | eff) String
+s str = do
+    return str
+
+s2 :: String -> forall eff. Eff (random :: RANDOM | eff) String
+s2 str = do
+    n <- random
+    return $ show n
+
+randomI n = do
+    r <- random
+    return (floor (r * (toNumber n)))
+
+
+blownOffCourse = do
+    direction <- randomI 6
+    distance <- randomI 5
+    return ("Blown off course, go " ++ (show $ distance + 1) ++ " hexes in direction " ++ (show $ direction + 1))
+
+allsWell = s "All's well."
+
 
 decks :: Array ChanceDeck
 decks = [
-    { title: "Sea", cards: ["All's well", "All's well", "Rats eat one point of stores"]},
-    { title: "Storms", cards: ["All's well", "Mast Damaged, miss a turn", "One point of stores lost", "Blown off course"]},
-    { title: "Island", cards: ["Replenshed supplies", "Ran aground, miss a turn"]}
+    { title: "Sea", cards: [allsWell, allsWell, s "Rats eat one point of stores"]},
+    { title: "Storms", cards: [allsWell, s "Mast Damaged, miss a turn", s "One point of stores lost overboard.", blownOffCourse]},
+    { title: "Island", cards: [s "Replenished supplies.", s "Ran aground, miss a turn."]}
     ]
 
-button :: String -> forall eff. Eff (console :: CONSOLE, dom :: DOM | eff) Node
+button :: String -> forall eff. Eff (dom :: DOM | eff) Node
 button title = do
     node <- createElement "button"
-
+    addClass "aui-button" node
+    setInnerHTML title node
     return node
 
-render :: Node -> ChanceDeck -> forall eff. Eff (console :: CONSOLE, dom :: DOM, random :: RANDOM | eff) Unit
+render :: Node -> ChanceDeck -> forall eff. Eff (dom :: DOM, random :: RANDOM | eff) Unit
 render node deck = do
-    buttonSpan <- createElement "button"
-    addClass "aui-button" buttonSpan
-    setInnerHTML deck.title buttonSpan
+    buttonNode <- button deck.title
     deckDiv <- createElement "p"
-    appendChild buttonSpan deckDiv
+    appendChild buttonNode deckDiv
     appendChild deckDiv node
-    addEventListener "click" (showCard deck deckDiv) buttonSpan
+    addEventListener "click" (showCard deck deckDiv) buttonNode
     return unit
 
-chooseCard :: ChanceDeck -> Number -> Maybe String
+indexWithDefault :: forall a. Array a -> Int -> a -> a
+indexWithDefault as i default =
+    case index as i of
+        Nothing -> default
+        Just a -> a
+
+chooseCard :: ChanceDeck -> Number -> forall eff. Eff (random :: RANDOM | eff) String
 chooseCard deck n =
     let
-      i = floor (n * (toNumber $ length deck.cards))
-    in
-      index deck.cards i
+        i = floor (n * (toNumber $ length deck.cards))
+    in do
+        indexWithDefault deck.cards i (s "error")
 
-showCard :: ChanceDeck -> Node -> forall eff. Eff (console :: CONSOLE, dom :: DOM, random :: RANDOM | eff) Unit
+--chooseCard2 :: ChanceDeck -> Number -> forall eff. Eff (random :: RANDOM | eff) String
+--chooseCard2 deck n =
+--    let
+--        i = floor (n * (toNumber $ length deck.cards))
+--    in
+--        case index deck.cards i of
+--            Nothing -> s "error"
+--            Just f -> f
+
+showCard :: ChanceDeck -> Node -> forall eff. Eff (dom :: DOM, random :: RANDOM | eff) Unit
 showCard deck node = do
     test <- createElement "span"
     i <- random
-    -- setInnerHTML (fromMaybe "Error" (chooseCard deck i)) test
-    -- appendChild test node
-    dialog { width: 400, height: 200, id: "card-dialog", closeOnOutsideClick: true, contents: fromMaybe "Error" (chooseCard deck i) }
+    text <- chooseCard deck i
+    dialog { width: 400, height: 200, id: "card-dialog", closeOnOutsideClick: true, contents: text }
     return unit
 
-setupEventHandlers :: forall eff. Eff (console :: CONSOLE, dom :: DOM, random :: RANDOM | eff) Unit
-setupEventHandlers = do
+renderDecks :: forall eff. Eff (dom :: DOM, random :: RANDOM | eff) Unit
+renderDecks = do
   Just chanceContainer <- querySelector "#chanceContainer"
   foldM (\u deck -> render chanceContainer deck) unit decks
-  -- render {title: "Test", cards: ["A", "B", "C", "D"]} chanceContainer
-  -- render {title: "Test2", cards: ["A", "B", "C", "D"]} chanceContainer
-
   return unit
 
-main :: forall eff. Eff (console :: CONSOLE, dom :: DOM, random :: RANDOM | eff) Unit
+main :: Eff (console :: CONSOLE, dom :: DOM, random :: RANDOM) Unit
 main = do
-  log "Attaching event handlers"
-  setupEventHandlers
+  renderDecks
